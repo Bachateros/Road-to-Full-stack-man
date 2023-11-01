690d0180402d033bdc8dc079369d7f68701be089
Bachateros <ser-morkovka47@mail.ru>
2023-11-01 16:26:42 +0300

    smth

--- ООП (Трансформеры)/js/Observer.js
+++ ООП (Трансформеры)/js/Observer.js
@@ -1,34 +1,40 @@
 import Render from "./Render.js";
 import State from "./State.js";
 
-export default class Subject{
-    constructor() {
-        this.subscribers = [];
+export default class Subject {
+  constructor() {
+    this.subscribers = [];
+  }
+  isChange(newEl) {
+    if (newEl.health != this.autobot || newEl.health != this.desiptikon) {
+      return true;
+    } else {
+      return false;
     }
-    isChange(newEl){
-        if(newEl.health != this.autobot || newEl.health != this.desiptikon){
-            return true;
-        } else {
-            return false;
-        }
-    }
-    checkChanges(){
-        this.subscribers.forEach( el => {
-            if( this.isChange(el) ){
-                this.notifyAll();
-            }
-        })
-    }
+  }
+  checkChanges() {
+    this.subscribers.forEach((el) => {
+      if (this.isChange(el)) {
+        this.notifyAll();
+      }
+    });
+  }
 
-    notifyAll() {
-        Render.rerend();
-        // return this.actions.forEach( subs => subs.inform(this) );
-    }
+  notifyAll() {
+    Render.rerend(); //return this.subscribers.forEach(subs => subs.rerender(this));
+    // return this.actions.forEach( subs => subs.inform(this) );
+  }
 
-    subscribe(observer) {
-        this.subscribers.push(observer);
-        this[observer] = observer;
-    }
+  subscribe(observer) {
+    this.subscribers.push(observer);
+    // this[observer] = observer;
+  }
+}
+
+class auotobotsHealth {
+  rerender() {
+    Render.rerend();
+  }
 }
 
 // const autoNews = new Subject();
@@ -36,7 +42,6 @@ export default class Subject{
 // autoNews.register(new Observer());
 
 // autoNews.setNews('lorem5');
-
 
 // class Observer{
 //     inform(message) {
