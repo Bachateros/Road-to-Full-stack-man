7d2cd1751072784f9870b0a7f4c40dcd07380fbf
Bachateros <ser-morkovka47@mail.ru>
2023-11-08 19:11:40 +0300

    made link with components tasks

--- ToDoFramework/ToDoAngularMonster/src/app/task-list/task-list.component.ts
+++ ToDoFramework/ToDoAngularMonster/src/app/task-list/task-list.component.ts
@@ -1,10 +1,35 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Provider, forwardRef } from '@angular/core';
+import { NG_VALUE_ACCESSOR, ControlValueAccessor } from '@angular/forms';
+const VALUE_ACCESSOR: Provider = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => TaskListComponent),
+  multi: true,
+};
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrls: ['./task-list.component.scss'],
+  providers: [VALUE_ACCESSOR],
 })
-export class TaskListComponent {
-  @Input() tasks!: Array<string>;
+export class TaskListComponent implements ControlValueAccessor {
+  tasks!: Array<string>;
+
+  private onChange = (value: any) => {};
+
+  writeValue(tasks: Array<string>): void {
+    this.tasks = tasks;
+  }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+  }
+  registerOnTouched(fn: any): void {}
+  setDisabledState?(isDisabled: boolean): void {}
+
+  deleteTask(task: string): void {
+    // console.log(`I delete ${id} - ${this.tasks[id]}`);
+
+    this.tasks = this.tasks.filter((el) => el != task);
+    this.onChange(this.tasks);
+  }
 }
